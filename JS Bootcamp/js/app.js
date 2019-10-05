25b790ed532961bb881f72f0c5ead877b4eaaba3
[InnCreator] <[engbethany4@gmail.com]>
2019-10-05 21:57:45 +0300

    goodnight

---
+++ JS Bootcamp/js/app.js
@@ -0,0 +1,7 @@
+let character = '';
+let timesToRepeat = 6;
+
+for (let i = 0; i < timesToRepeat; i++){
+    character += 'a'
+}
+console.log(character)
--- JS Bootcamp/js/script.js
+++ JS Bootcamp/js/script.js
@@ -1,9 +1,11 @@
+// variables
 const myName = 'Bethany';
 const sentence = 'Hi ' + myName + '! How are you?'
 const sentenceWithTemplate = `Hello ${myName}! How are you?`
 console.log(sentence);
 console.log(sentenceWithTemplate);
 
+// if loops
 const RuheniIsMad = false;
 if (RuheniIsMad) {
     console.log('stay away')
@@ -11,9 +13,21 @@ if (RuheniIsMad) {
 else {
     console.log('Tease him till he is mad')
 }
-var character = 'a';
-const timesToRepeat = 6;
+/*
+  Write some code that declares two variables, character and timesToRepeact.
+  Using a loop, repeat that character that many times and then console.log it.
+  Example, if I had character = 'f' and timesToRepeat = 5, it'd console.log
+  'fffff'
+*/
+let character = '';
+let timesToRepeat = 6;
+
+for (let i = 0; i < timesToRepeat; i++){
+    character += 'a';
+}
+console.log(character);
+
 while (character.length <= timesToRepeat) {
-    character += character;
+    character += 'a';
 }
 console.log(character);
