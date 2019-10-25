262b04dd12ff1c7f1d0713106c7cb3ed4589bf00
[InnCreator] <[engbethany4@gmail.com]>
2019-10-25 11:44:04 +0300

    sweet

--- Closed Door/script.js
+++ Closed Door/script.js
@@ -4,11 +4,21 @@ let doorImage3 = document.getElementById('door3');
 const botDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
 const beachDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
 const spaceDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
-numClosedDoors = 3;
-let openDoor1 = ;
-let openDoor2 =  ;
-let openDoor3 = ;
+let numClosedDoors = 3;
+const startButton = document.getElementById('start');
+let openDoor1;
+let openDoor2;
+let openDoor3;
+let currentlyPlaying = true;
 let closedDoorPath = 'https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg';
+const isBot = (door) => {
+  if(door.src === botDoorPath){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
 const isClicked = (door) => {
   if(door.src = closedDoorPath){
     return false;
@@ -20,6 +30,9 @@ const isClicked = (door) => {
 const playDoor = () => {
   numClosedDoors--;
   if(numClosedDoors === 0){
+    gameOver('win');
+  }
+  else if(isBot(door)){
     gameOver();
   }
 }
@@ -41,22 +54,45 @@ const randomChoreDoorGenerator = () => {
     openDoor1 = beachDoorPath;
   }
 }
-if(!isClicked(doorImage1)){
-  doorImage1.onclick = () => {
+if(currentlyPlaying && !isClicked(doorImage1)){
+  doorImage1.onclick = (playDoor(doorImage1)) => {
   doorImage1.src = openDoor1;
   playDoor();
 }
 }
-if(!isClicked(doorImage2)){
-  doorImage2.onclick = () => {
+if(currentlyPlaying && !isClicked(doorImage2)){
+  doorImage2.onclick = (playDoor(doorImage2)) => {
   doorImage2.src = openDoor2;
   playDoor();
 }
 }
-if(!isClicked(doorImage3)){
-  doorImage3.onclick = () => {
+if(currentlyPlaying && !isClicked(doorImage3)){
+  doorImage3.onclick = (playDoor(doorImage3)) => {
   doorImage3.src = openDoor3;
   playDoor();
 }
 }
-randomChoreDoorGenerator();
+if(!currentlyPlaying){
+  startButton.onclick = () => {
+  startsRound()
+}
+}
+const startRound = () => {
+  doorImage1.src = closedDoorPath;
+  doorImage2.src = closedDoorPath;
+  doorImage3.src = closedDoorPath;
+  numClosedDoors = 3;
+  startButton.innerHTML = 'Good Luck!'
+  currentlyPlayin = true;
+  randomChoreDoorGenerator();
+}
+const gameOver = (status) => {
+  if(status === 'win'){
+    startButton.innerHTML = 'You win! Play Again?'
+  }
+  else{
+    startButton.innerHTML = 'Game Over! Play Again?'
+  }
+  currentlyPlaying = false;
+}
+startRound();
